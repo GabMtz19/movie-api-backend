4eb923d2b86f2b8a91e832d0f2792fdec75be8c9
Gabriel Martinez <gabjr2001@hotmail.com>
2023-08-09 17:54:41 -0600

    Innitial Commit

---
+++ src/main/java/com/example/movies/MoviesApplication.java
@@ -0,0 +1,28 @@
+package com.example.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+@RestController
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/v1/movies").allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
+
+}
